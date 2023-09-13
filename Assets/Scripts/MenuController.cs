10733f387d995cf62090d7e69720c2de9225fd16
Grégoire LE ROUX <gregoire.leroux.pro@gmail.com>
2023-09-13 12:15:22 +0200

    Add main menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public Button StartButton, AboutButton, QuitButton, BackButton;
+    public GameObject MainSection, AboutSection;
+
+
+    private void Awake()
+    {
+        SetupButtons();
+    }
+
+    private void SetupButtons()
+    {
+        StartButton.onClick.AddListener(() =>
+        {
+            PlayGame();
+        });
+        AboutButton.onClick.AddListener(() =>
+        {
+            ShowAboutScene(true);
+        });
+        BackButton.onClick.AddListener(() =>
+        {
+            ShowAboutScene(false);
+        });
+        QuitButton.onClick.AddListener(() =>
+        {
+            QuitGame();
+        });
+    }
+
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("ExampleVRScene");
+    }
+
+    public void ShowAboutScene (bool show)
+    {
+        MainSection.SetActive(!show);
+        AboutSection.SetActive(show);
+    }
+
+    public void QuitGame ()
+    {
+        bool quit = false;
+        #if UNITY_EDITOR
+            quit = true;
+            EditorApplication.ExitPlaymode();
+        #endif
+        if (!quit) Application.Quit();
+    }
+}
