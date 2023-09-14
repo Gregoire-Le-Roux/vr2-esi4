a335cac8ef26f7915ba47c79238dff1ed3657faf
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-09-14 13:46:40 +0200

    fix: remove bug

--- Assets/DollScript.cs
+++ Assets/DollScript.cs
@@ -11,23 +11,9 @@ public class DollScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        //if (collision.gameObject.CompareTag("Coeur"))
-        //{
-        //    doorAnimatorController.SetBool("ouverture", true);
-
-        //    // Load the music file
-        //    music = Resources.Load<AudioClip>("Assets/Horror Elements/Ambient/Amb_scary.wav");
-
-        //    // Play the music
-        //    AudioSource audioSource = GetComponent<AudioSource>();
-        //    audioSource.PlayOneShot(music);
-
-        //    // Stop the scene-of-murder audio clip
-        //    AudioSource sceneOfMurderAudioSource = GetComponentInChildren(gameObject, "sceneOfMurderAudioSource");
-        //    if (sceneOfMurderAudioSource != null && sceneOfMurderAudioSource.CompareTag("sceneOfMurderAudioSource"))
-        //    {
-        //        sceneOfMurderAudioSource.Stop();
-        //    }
-        //}
+        if (collision.gameObject.CompareTag("Coeur"))
+        {
+            doorAnimatorController.SetBool("ouverture", true);
+        }
     }
 }
