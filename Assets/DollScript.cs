b48977e22a720cb20151f65916bb56af4b1ac848
LouisMaupas <58144475+LouisMaupas@users.noreply.github.com>
2023-09-14 10:43:07 +0200

    script ok

--- Assets/DollScript.cs
+++ Assets/DollScript.cs
@@ -6,11 +6,12 @@ public class SphereCollisionDetection : MonoBehaviour
 {
     public GameObject Door;
     public Animator doorAnimatorController;
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Coeur"))
         {
-         //   doorAnimatorController.Play("doorAnimationClosing");
+            doorAnimatorController.SetBool("ouverture", true);
         }
     }
 }
